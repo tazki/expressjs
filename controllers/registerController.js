4d265f4eb914be9cb262a0a3080f600ec3cc2b44
Mark Bautista <42196376+tazki@users.noreply.github.com>
2024-07-17 22:23:58 +0800

    [Update] Implement MongoDB to User & Employee Schema

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,38 +1,27 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-const fsPromises = require("fs").promises;
-const path = require("path");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
-  const { username, pwd } = req.body;
-  if (!username || !pwd)
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
     return res
       .status(400)
       .json({ message: "Username and password are required." });
 
-  const existingUser = usersDB.users.find((user) => user.username === username);
+  const existingUser = await User.findOne({ username: user }).exec();
   if (existingUser)
     return res.status(409).json({ message: "Username is taken" }); // Conflict
 
   try {
     const hashedPwd = await bcrypt.hash(pwd, 10);
-    const newUser = {
-      username: username,
-      roles: { User: 2001 },
+    // create and store the new user
+    const result = await User.create({
+      username: user,
       password: hashedPwd,
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
-    console.log(usersDB.users);
-    res.status(201).json({ success: `New user ${username} created!` });
+    });
+    console.log(result);
+
+    res.status(201).json({ success: `New user ${user} created!` });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
